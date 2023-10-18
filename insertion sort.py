e490d60eb5ec7df283b9d710b6da9463e222111c
Muhammad Daniyal Alam <142598280+mdaniyal-alam@users.noreply.github.com>
2023-10-18 10:17:06 -0700

    Update insertion sort.py

--- insertion sort.py
+++ insertion sort.py
@@ -4,7 +4,7 @@ for bluep in range(1, len(array)):
     value = array[bluep]
     blackp = bluep - 1
 
-    while array[blackp] > array[bluep] and blackp >= 0:
+    while array[blackp] > value and blackp >= 0:
         array[blackp + 1] = array[blackp]
         blackp -= 1
     array[blackp + 1] = value
